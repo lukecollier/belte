68baf066544076404d987a35aa970b6d775627c4
Luke Collier <leccollier@gmail.com>
2019-03-28 23:05:44 +0000

    - pulled over old version of react loader

--- src/loader/react.js
+++ src/loader/react.js
@@ -1,9 +1,23 @@
+import ReactDOMServer from 'react-dom/server';
+import React, { Component } from 'react';
 import * as rollup from 'rollup';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
-export async function loader(src, { encodeForFilename, encodeForVariableName }) {
+export const render = (src, attr) => {
+	const Component = require(src).default;
+	const comp = React.createElement(Component, attr);
+	return ReactDOMServer.renderToString(comp)
+}
+
+export const client = (src, encode) => {
+  const comp = readFileSync(src, 'utf8');
+	// walk the tree, remove imports for react, reactDOm that will be included globally
+	return `React.createElement(HelloMessage, { name: "Taylor" })`
+}
+
+export async function loaderTest(src, { encodeForFilename, encodeForVariableName }) {
   const inputOptions = {
     input: src,
     external: (_) => true,
@@ -41,3 +55,24 @@ export async function loader(src, { encodeForFilename, encodeForVariableName }) 
     ]
   }
 }
+
+// way to check if a component is a react component for dependencies
+// Component.prototype 
+// 	&& Component.prototype.isReactComponent 
+export const loader = (src, encode) => {
+  const data = readFileSync(src, 'utf8');
+  const dir = pathUtil.dirname(src);
+  const name = encode(data);
+
+  const compDeps = () => deps(src).filter(filepath => filepath.endsWith('.html')); 
+  const otherDeps = () => deps(src).filter(filepath => !filepath.endsWith('.html')); 
+
+  return {
+    render: R.partial(render, [src]),
+    client: R.partial(client, [dir, data, encode]),
+    styles: R.partial(style, [data]),
+    constructor: R.partial(constructor, [name]),
+    dependencies: compDeps,  
+    otherDependencies: otherDeps,  
+  }
+};
